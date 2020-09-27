269dc0ee5607820a778a48824059a4bd64c93202
sharkyBy <valik_1973@mail.ru>
2020-09-27 19:29:20 +0200

    add new file a todo list

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="container">
+        <ul className="todoList">
+          <li className="todoListTitle">TODO LIST</li>
+          <li className="todoListItem">
+            <p>Task 1</p>
+            <div className="button">
+              <button id="done"> Done</button>
+              <button id="delete">Delete</button>
+            </div>
+          </li>
+        </ul>
+        <button id="add" type="button">Add</button>
+      </div>
     </div>
   );
 }
